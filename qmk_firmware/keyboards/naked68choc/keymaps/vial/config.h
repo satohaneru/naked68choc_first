1857270d33d95b36fe2f89a2158ee0c85d07bfae
さとーはねる <123873735+satohaneru@users.noreply.github.com>
2025-11-23 14:47:26 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/naked68choc/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x46, 0x64, 0x42, 0xeb, 0x51, 0x16, 0x1d, 0x6a}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
